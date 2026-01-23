114bcb21ebddee3edb4789f3ada9e2719279ee1d
Paolo Leggio <paolo@digitalmonkeys.it>
2026-01-23 16:01:25 +0100

    added convertJSObjectToMIMEHeader

--- client/client.go
+++ client/client.go
@@ -19,6 +19,36 @@ type EmailClient struct {
 	client   *client.Client
 }
 
+// convertJSObjectToMIMEHeader converte un oggetto JavaScript in textproto.MIMEHeader
+// Gestisce la conversione da map[string]interface{} (come viene passato da k6) a textproto.MIMEHeader
+func convertJSObjectToMIMEHeader(obj map[string]interface{}) textproto.MIMEHeader {
+	header := make(textproto.MIMEHeader)
+	
+	for key, value := range obj {
+		switch v := value.(type) {
+		case []interface{}:
+			// Array di valori JavaScript
+			values := make([]string, 0, len(v))
+			for _, item := range v {
+				if str, ok := item.(string); ok {
+					values = append(values, str)
+				}
+			}
+			if len(values) > 0 {
+				header[key] = values
+			}
+		case string:
+			// Singola stringa
+			header[key] = []string{v}
+		case []string:
+			// Già un array di stringhe (caso raro ma possibile)
+			header[key] = v
+		}
+	}
+	
+	return header
+}
+
 func (e *EmailClient) Login() string {
 	c, err := client.DialTLS(e.Url+":"+strconv.Itoa(e.Port), nil)
 
@@ -38,13 +68,16 @@ func (e *EmailClient) Login() string {
 
 }
 
-func (e *EmailClient) Read(header textproto.MIMEHeader) (string, string) {
+func (e *EmailClient) Read(headerObj map[string]interface{}) (string, string) {
 	_, err := e.client.Select("INBOX", true)
 
 	if err != nil {
 		fmt.Println(err)
 		return "", err.Error()
 	}
+
+	// Converti l'oggetto JavaScript in textproto.MIMEHeader
+	header := convertJSObjectToMIMEHeader(headerObj)
 
 	criteria := &imap.SearchCriteria{
 		Header: header,
@@ -55,6 +88,10 @@ func (e *EmailClient) Read(header textproto.MIMEHeader) (string, string) {
 	if err != nil {
 		fmt.Println(err)
 		return "", err.Error()
+	}
+
+	if len(ids) == 0 {
+		return "", "No messages found"
 	}
 
 	seqSet := new(imap.SeqSet)
@@ -80,6 +117,10 @@ func (e *EmailClient) Read(header textproto.MIMEHeader) (string, string) {
 	section, _ := imap.ParseBodySectionName("BODY[TEXT]")
 	r := msg.GetBody(section)
 
+	if r == nil {
+		return "", "Could not get message body"
+	}
+
 	qr := quotedprintable.NewReader(r)
 	bs, err := ioutil.ReadAll(qr)
 
@@ -92,5 +133,7 @@ func (e *EmailClient) Read(header textproto.MIMEHeader) (string, string) {
 }
 
 func (e *EmailClient) Logout() {
-	e.client.Logout()
+	if e.client != nil {
+		e.client.Logout()
+	}
 }
--- imap.go
+++ imap.go
@@ -19,9 +19,39 @@ func init() {
 
 type Imap struct{}
 
+// convertJSObjectToMIMEHeader converte un oggetto JavaScript in textproto.MIMEHeader
+// Gestisce la conversione da map[string]interface{} (come viene passato da k6) a textproto.MIMEHeader
+func convertJSObjectToMIMEHeader(obj map[string]interface{}) textproto.MIMEHeader {
+	header := make(textproto.MIMEHeader)
+	
+	for key, value := range obj {
+		switch v := value.(type) {
+		case []interface{}:
+			// Array di valori JavaScript
+			values := make([]string, 0, len(v))
+			for _, item := range v {
+				if str, ok := item.(string); ok {
+					values = append(values, str)
+				}
+			}
+			if len(values) > 0 {
+				header[key] = values
+			}
+		case string:
+			// Singola stringa
+			header[key] = []string{v}
+		case []string:
+			// Già un array di stringhe (caso raro ma possibile)
+			header[key] = v
+		}
+	}
+	
+	return header
+}
+
 // Simple function for one time read
 // Use EmailClient for more complex needs
-func (*Imap) Read(email, password, URL string, port int, header textproto.MIMEHeader) (string, string) {
+func (*Imap) Read(email, password, URL string, port int, headerObj map[string]interface{}) (string, string) {
 	c, err := client.DialTLS(URL+":"+strconv.Itoa(port), nil)
 
 	if err != nil {
@@ -40,6 +70,9 @@ func (*Imap) Read(email, password, URL string, port int, header textproto.MIMEHe
 		return "", err.Error()
 	}
 
+	// Converti l'oggetto JavaScript in textproto.MIMEHeader
+	header := convertJSObjectToMIMEHeader(headerObj)
+
 	criteria := &imap.SearchCriteria{
 		Header: header,
 	}
@@ -48,6 +81,10 @@ func (*Imap) Read(email, password, URL string, port int, header textproto.MIMEHe
 
 	if err != nil {
 		return "", err.Error()
+	}
+
+	if len(ids) == 0 {
+		return "", "No messages found"
 	}
 
 	seqSet := new(imap.SeqSet)
@@ -66,11 +103,15 @@ func (*Imap) Read(email, password, URL string, port int, header textproto.MIMEHe
 	msg := <-messages
 
 	if msg == nil {
-		return "", err.Error()
+		return "", "No message"
 	}
 
 	section, _ := imap.ParseBodySectionName("BODY[TEXT]")
 	r := msg.GetBody(section)
+
+	if r == nil {
+		return "", "Could not get message body"
+	}
 
 	qr := quotedprintable.NewReader(r)
 	bs, err := ioutil.ReadAll(qr)
